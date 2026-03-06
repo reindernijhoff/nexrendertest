cc1ca76b1324b8f07d353f62c1a863b953c619a2
Reinder Nijhoff <reinder@mediamonks.com>
2026-03-06 11:19:02 +0100

    more

---
+++ test.js
@@ -0,0 +1,28 @@
+const { render } = require('@nexrender/core')
+const job  = require('./testjob.json')
+
+function uppercaseFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+const main = async () => {
+    const names = ['freek', 'mart', 'mark', 'reinder'];
+    const greetings = ['hi', 'hello', 'welcome', 'greetings', 'hey'];
+    const count = 20;
+
+    for (let i = 0; i < count; i++) {
+        const name = names[i % names.length];
+        const greeting = greetings[i % greetings.length];
+        const color = [Math.random(), Math.random(), Math.random()];
+        const max = Math.max(...color);
+        const normalizedColor = color.map(c => c / max);
+
+        job.assets[0].src = `file:///git/nexrendertest/${name}.jpg`;
+        job.assets[1].value = `${greeting} ${uppercaseFirst(name)}!`;
+        job.assets[2].value = normalizedColor;
+        job.actions.postrender[1].output = `C:/git/nexrendertest/results/output_${`${i}`.padStart(2, '0')}.mp4`;
+        const result = await render(job);
+    }
+}
+
+main().catch(console.error);
